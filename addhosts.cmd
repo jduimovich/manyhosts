89e162978862014866e8d56ee21b5d9e3ebead26
John Duimovich <$MY_GITHUB_EMAIL>
2021-11-16 22:08:17 -0500

    first commit

---
+++ addhosts.cmd
@@ -0,0 +1,27 @@
+
+set BIN=g:\Dev\admin-helper\cmd\admin-helper\admin-helper.exe
+set HOSTS=C:\Windows\System32\drivers\etc\hosts
+
+%BIN% add 127.0.0.1 host1.crc.testing
+type %HOSTS% 
+%BIN% add 127.0.0.1 host2.crc.testing
+type %HOSTS% 
+%BIN% add 127.0.0.1 host3.crc.testing
+type %HOSTS% 
+%BIN% add 127.0.0.1 host4.crc.testing
+type %HOSTS% 
+%BIN% add 127.0.0.1 host5.crc.testing
+type %HOSTS% 
+%BIN% add 127.0.0.1 host6.crc.testing
+type %HOSTS% 
+%BIN% add 127.0.0.1 host7.crc.testing
+type %HOSTS% 
+%BIN% add 127.0.0.1 host8.crc.testing
+type %HOSTS% 
+%BIN% add 127.0.0.1 host9.crc.testing
+type %HOSTS% 
+%BIN% add 127.0.0.1 host10.crc.testing
+type %HOSTS% 
+%BIN% add 127.0.0.1 host11.crc.testing
+type %HOSTS% 
+ 
---
+++ removehosts.cmd
@@ -0,0 +1,27 @@
+set BIN=g:\Dev\admin-helper\cmd\admin-helper\admin-helper.exe
+set HOSTS=C:\Windows\System32\drivers\etc\hosts
+
+%BIN% remove   host1.crc.testing 
+type %HOSTS% 
+%BIN% remove  host2.crc.testing 
+type %HOSTS% 
+%BIN% remove   host3.crc.testing 
+type %HOSTS% 
+%BIN% remove  host4.crc.testing 
+type %HOSTS% 
+%BIN% remove  host5.crc.testing 
+type %HOSTS% 
+%BIN% remove   host6.crc.testing 
+type %HOSTS% 
+%BIN% remove   host7.crc.testing 
+type %HOSTS% 
+%BIN% remove   host8.crc.testing 
+type %HOSTS% 
+%BIN% remove   host9.crc.testing 
+type %HOSTS% 
+%BIN% remove   host10.crc.testing 
+type %HOSTS% 
+%BIN% remove   host11.crc.testing
+type %HOSTS% 
+
+
